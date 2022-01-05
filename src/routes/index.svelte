e317494dd01864bfdb17c77b63153bfa9094673e
Kartik Rao <kr40.git@gmail.com>
2022-01-05 11:43:09 +0530

    added about page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,9 @@
 	<title>SvelteKit + TailwindCSS</title>
 </svelte:head>
 
-<h1 class="text-center text-4xl text-red-600">Welcome to Svelte Kit + TailwindCSS Template</h1>
+<h1 class="text-4xl text-center text-red-600">Welcome to Svelte Kit + TailwindCSS Template</h1>
 
-<p class="my-10 text-center text-xl">
+<p class="my-10 text-xl text-center">
 	This is a barebones template that comes pre-configured with TailwindCSS and eslint for starting SvelteKit projects.
 	Please make sure you have the Svelte for VSCode plugin installed. It provides prettier rules and even contains a
 	eslint linter.
